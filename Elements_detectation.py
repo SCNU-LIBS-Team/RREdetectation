5e344f8fcff07cd0e111547bcfdf4508289ebb03
Noob-12345 <3295776642@qq.com>
2026-03-12 01:31:00 +0800

    检测了Pr和Ce
    Pr和Ce都属于线复杂，并且谱线的相对强度很低的

--- Elements_detectation.py
+++ Elements_detectation.py
@@ -631,9 +631,9 @@ signal_path2= r'D:\LIBS\RREdetectation\Rareearth\Spectrum' #待测光谱路径
 signal_path3= r'D:\LIBS\RREdetectation\RREs' #待测光谱路径
 I_file_list = glob.glob(os.path.join(signal_path3, "*.csv"))
 I_elements_list = [os.path.splitext(os.path.basename(f))[0] for f in I_file_list]
-target_files=['03116_95'] #待测光谱文件名列表（不带扩展名）
+target_files=['07129_95'] #待测光谱文件名列表（不带扩展名）
 
-target_element='Y'
+target_element='Pr'
 specifybotton = True  # True: 遍历全部文件，仅输出目标元素；False: 只跑 target_files，输出全部元素
 plotbotton=True #是否绘图展示Boltzmann图
 plottarget='PrII' #Boltzmann图绘制目标元素
