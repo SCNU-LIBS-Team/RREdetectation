9cbac4e8e483547355998aeea8501d5071b53f85
Noob-12345 <153294546+Noob-12345@users.noreply.github.com>
2026-04-05 22:44:37 +0800

    完成Pt20-25的随机光谱生成

--- SimspecGen/Gen.py
+++ SimspecGen/Gen.py
@@ -225,5 +225,5 @@ def run_generate_with_auto_restart(retry_wait_seconds=10,mode='generate',file_na
             time.sleep(retry_wait_seconds)
 
 
-generate_spectra(mode='debug',file_name="070036",resolution=3000,Te=0.75,Ne=1e17,low_w=200,upper_w=900)
-# run_generate_with_auto_restart(retry_wait_seconds=10,mode='debug',file_name="070099")
+#generate_spectra(mode='debug',file_name="070036",resolution=3000,Te=0.75,Ne=1e17,low_w=200,upper_w=900)
+run_generate_with_auto_restart(retry_wait_seconds=10,mode='random',file_name="070099")
